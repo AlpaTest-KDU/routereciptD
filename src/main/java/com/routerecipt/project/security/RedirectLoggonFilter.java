324d8ea4e6b22f928c5331f51f84011be2df5683
dongun <tryduddnd@naver.com>
2025-12-09 16:34:50 +0900

    Restore resources + update security files

--- src/main/java/com/routerecipt/project/security/RedirectLoggonFilter.java
+++ src/main/java/com/routerecipt/project/security/RedirectLoggonFilter.java
@@ -1,5 +1,42 @@
 package com.routerecipt.project.security;
 
-public class RedirectLoggonFilter {
+import java.io.IOException;
 
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Order(1)
+public class RedirectLoggonFilter extends OncePerRequestFilter {
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		if ("/login".equals(request.getRequestURI()) && request.getMethod().equalsIgnoreCase("POST")) {
+			
+			String username = request.getParameter("u_id");
+			String password = request.getParameter("u_pw");
+			
+			System.out.println("====로그인 요청 발생====");
+			System.out.println("입력한 ID" + username);
+			System.out.println("입력한 PW" + (password != null ? "*".repeat(password.length()) : null));
+			System.out.println("===================");
+			
+		}
+		
+		filterChain.doFilter(request, response);
+		
+		int status = response.getStatus();
+		if (status == HttpServletResponse.SC_FOUND) {
+			
+		}
+		
+	}
 }
