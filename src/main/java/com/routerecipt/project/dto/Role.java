7d67db074d70383235a5cba15726cc08f741ac07
dongun <tryduddnd@naver.com>
2025-12-10 11:33:34 +0900

    Role파일 작성 완료(동운)

--- src/main/java/com/routerecipt/project/dto/Role.java
+++ src/main/java/com/routerecipt/project/dto/Role.java
@@ -1,5 +1,13 @@
 package com.routerecipt.project.dto;
 
-public enum Role {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public enum Role {
+	ROLE_USER("ROLE_USER"),
+	ROLE_ADMIN("ROLE_ADMIN");
+	
+	private final String role;
 }
