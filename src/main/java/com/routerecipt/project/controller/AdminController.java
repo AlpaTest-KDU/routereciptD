c65cd5501b292adb640225ec03f6c61960072a5c
이희준 <dhmkjeil@gmail.com>
2025-12-10 10:01:19 +0900

    [이희준] 1. AdminController 메소드 뼈대 작성

--- src/main/java/com/routerecipt/project/controller/AdminController.java
+++ src/main/java/com/routerecipt/project/controller/AdminController.java
@@ -1,8 +1,36 @@
 package com.routerecipt.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.routerecipt.project.service.ReceiptServiceImp;
+import com.routerecipt.project.service.UserServiceImp;
 
 @Controller
 public class AdminController {
-
+	
+	@Autowired
+	private UserServiceImp userServiceImp;
+	private ReceiptServiceImp receiptServiceImp;
+	
+	@DeleteMapping("adminPage/userInfoDelete/{id}")
+	public String userInfoDelete(@PathVariable(value="id") String id) {
+		return "adminPage";
+	}
+	
+	@GetMapping("/userReceiptInfoPage/userReceiptInfoSelect")
+	public String userReceiptInfoSelect(
+				@RequestParam(value = "year") int year,
+				@RequestParam(value = "month") int month) {
+		return "userReceiptInfoPage";
+	}
+	
+	@GetMapping("/userReceiptInfoPage/userInfoSelect")
+	public String userInfoSelect(@RequestParam(value = "userName") String userName) {
+		return "userReceiptInfoPage";
+	}
 }
