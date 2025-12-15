26623f7a815be9930b6fef8fb0936e52e229446b
이희준 <dhmkjeil@gmail.com>
2025-12-15 10:28:20 +0900

    [이희준] 1. 유저 삭제부분 Script 파일로 따로 빼서 작성

---
+++ src/main/resources/static/js/userInfoDelete.js
@@ -0,0 +1,19 @@
+function confirmDelete() {
+    const userIdElement = document.getElementById('currentUserId');
+    if (!userIdElement) return;
+
+    const currentUserId = userIdElement.value;
+    const userInput = prompt("삭제하시려면 아이디('${currentUserId}')를 입력하세요.");
+    
+    if (userInput === null) {
+        return;
+    }
+
+    if (userInput === currentUserId) {
+        if (confirm("정말로 탈퇴하시겠습니까? 모든 정보가 삭제됩니다.")) {
+            document.getElementById('deleteForm').submit();
+        }
+    } else {
+        alert("아이디가 일치하지 않습니다.");
+    }
+}
