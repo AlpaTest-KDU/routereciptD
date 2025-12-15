30b3f3f220c86e2de854a5b19c7f359d28fc805a
이희준 <dhmkjeil@gmail.com>
2025-12-15 15:36:57 +0900

    [이희준] JS 문구를 템플릿 리터럴로 수정

--- src/main/resources/static/js/userInfoDelete.js
+++ src/main/resources/static/js/userInfoDelete.js
@@ -3,7 +3,7 @@ function confirmDelete() {
     if (!userIdElement) return;
 
     const currentUserId = userIdElement.value;
-    const userInput = prompt("삭제하시려면 아이디('${currentUserId}')를 입력하세요.");
+    const userInput = prompt(`삭제하시려면 아이디('${currentUserId}')를 입력하세요.`);
     
     if (userInput === null) {
         return;
