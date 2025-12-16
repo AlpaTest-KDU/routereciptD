b2f72df18393e3cf0697a4524f16fa63c76a95a9
이희준 <dhmkjeil@gmail.com>
2025-12-16 17:16:43 +0900

    [이희준] 1. NoticeController RestAPI 수정

--- src/main/java/com/routerecipt/project/controller/NoticeController.java
+++ src/main/java/com/routerecipt/project/controller/NoticeController.java
@@ -2,6 +2,7 @@ package com.routerecipt.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,19 +11,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.routerecipt.project.service.NoticeServiceImp;
 
 @Controller
-@RequestMapping("/noticePage")
+@RequestMapping("/notice")
 public class NoticeController {
 	
 	@Autowired
 	private NoticeServiceImp noticeServiceImp;
 	
-	@PostMapping("/noticeRegister")
-	public String noticeRegister() {
-		return "noticePage";
+	// 공지사항 등록 화면
+	@GetMapping("/noticeRegisterPage")
+	public String noticeRegisterPage() {
+		return "notice/noticeRegisterPage";
+	}
+
+	// 공지사항 등록 기능
+	@PostMapping("/noticeRegist")
+	public String noticeRegist() {
+		return "notice/noticePage";
 	}
 	
-	@GetMapping("/noticeShow/{noticeTitle}")
-	public String noticeShow(@RequestParam("noticeTitle") String noticeTitle) {
-		return "noticePage";
+	// 공지사항 검색 기능
+	// 1. 검색란이 비어 있으면 모든 공지사항을 보여줌
+	// 2. 검색을 하면 해당하는 단어가 포함된 제목을 가진 공지사항을 보여줌
+	@PostMapping("/noticeSearch")
+	public String noticeSearch(Model model, @RequestParam(value="title", required=false) String title) {
+		return "notice/noticePage";
 	}
 }	
