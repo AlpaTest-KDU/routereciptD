ff7cea5d83d36eff4d8efbf4167c88af2a9e27e0
dongun <tryduddnd@naver.com>
2025-12-30 19:37:55 +0900

    [김동운]1. 카테고리 선별 딥러닝모델 연동 완료 2.pull시 properties각 환경에 맞게 수정 필요

--- src/main/java/com/routerecipt/project/config/RestTemplateConfig.java
+++ src/main/java/com/routerecipt/project/config/RestTemplateConfig.java
@@ -1,14 +1,14 @@
-package com.routerecipt.project.OpenAI;
+package com.routerecipt.project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class HttpConfig {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+public class RestTemplateConfig {
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- src/main/java/com/routerecipt/project/dto/AiCategoryResponse.java
+++ src/main/java/com/routerecipt/project/dto/AiCategoryResponse.java
@@ -1,10 +1,12 @@
 package com.routerecipt.project.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class AiCategoryResponse {
 	private String category;
 	private double confidence;
